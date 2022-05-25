bfb905c1498bfb4e7ea21faea22be1b4f5cdd18d
TuongNM <ng.manhtuong@gmail.com>
2022-05-25 11:22:06 +0700

    stric mode with age without var

---
+++ strict-mode.js
@@ -0,0 +1,17 @@
+// 'use strict';
+var fullName = "Nguyen Van A"
+
+function testFunc(){
+   age = 18;
+   
+}
+
+testFunc();
+
+console.log(fullName)
+console.log(age)
+
+//strict mode o dau ham 
+// strict mode o dau doan mo the script 
+// strict mode o dau file
+
