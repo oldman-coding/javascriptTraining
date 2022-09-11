837539566b264bbe78696700f60cfc20606be19d
oldmanCoding <sanhquan68@gmail.com>
2022-09-11 15:40:20 +0700

    refactor add event listener

--- html-css/W3-Band/popUp.js
+++ html-css/W3-Band/popUp.js
@@ -1,21 +1,31 @@
 function popUpShow() {
-  var popup = document.getElementById('popup');
-  if (popup) 
-    {console.log("Found")}
-  else 
-    {console.log("not found");} 
-
-  popup.classList.toggle("popup--show");
-  console.log("showed Popup");
+  var popup = document.getElementById('modal');
+  popup.classList.toggle("modal--open");
+  if (popup.classList.contains("modal--open")) {
+    console.log("Close)")
+  } else {
+    console.log("Open");
+  }
 }
 
-function removePopUp() {
-    var popup = document.getElementById('popup');
-    if (popup) 
-      {console.log("Found")}
-    else 
-      {console.log("not found");} 
-  
-    popup.classList.toggle("popup--show");
-    console.log("showed Popup");
-  } 
+const btns = document.querySelectorAll(".js-btn-modal"); 
+const modal = document.querySelector('.js-modal');
+const modal_body = document.querySelector('.js-modal-body');
+const modal_close = document.querySelectorAll('.js-popup-close');
+
+if (btns) {
+  for (btn of btns) {
+    btn.addEventListener("click", popUpShow());
+  }
+};
+
+for (close of modal_close) {
+  close.addEventListener("click", popUpShow());
+}
+
+modal.addEventListener("click", popUpShow());
+
+
+modal_body.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
